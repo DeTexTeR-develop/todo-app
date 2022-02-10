aceee54866897e707b02d9f72d26c79cdbb096ad
DeTexTeR-develop <ujjwalsingh90official@gmail.com>
2022-02-10 17:45:07 +0530

    connected firebase database

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const submit = document.querySelector("#form")
 const todoInput = document.querySelector("#inputInTodo");
+const todoItemList  = document.querySelector("#todoItems");
 
 
 submit.addEventListener('submit', function(e){
     e.preventDefault();
     console.log(todoInput.value);
+    db.collection("todo-items").add({
+        Text: todoInput.value,
+        status : "active"
+    })
     todoInput.value = "";
+    
 }) 
 
 
