16718ffcbe321c78d6c1d7c0594bd542315aa4cf
DeTexTeR-develop <ujjwalsingh90official@gmail.com>
2022-02-11 20:53:01 +0530

    completed firebase, fetching data from firebase store now

--- script.js
+++ script.js
@@ -11,7 +11,43 @@ submit.addEventListener('submit', function(e){
         status : "active"
     })
     todoInput.value = "";
-    
 }) 
 
+function getItems(){
+    db.collection("todo-items").onSnapshot((snapshot)=>{
+        console.log(snapshot);
+        let items = [];
+        snapshot.docs.forEach((doc) => {
+            items.push({
+                id: doc.id,
+                ...doc.data()
+            })
+        });
+        generateItems(items);
+    })
+}
+
+function generateItems(items){
+
+    let itemsHTML = "";
+    items.forEach((item) => {
+        console.log(item);
+        itemsHTML += `
+            <div class="todoItem" >
+                <div class="check">
+                    <div class="checkmark">
+                        <img src="./assets/icon-check.svg" alt="Icon for check">
+                    </div>
+                </div>
+                <div class="todoText">
+                    ${item.Text}
+                </div>
+            </div>
+        
+        `
+    })
+    document.querySelector(".todoItems").innerHTML = itemsHTML;
+}
+
+getItems();
 
